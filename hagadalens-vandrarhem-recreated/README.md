c7582f6340efcde233430315204d4a8ba29aeb4c
andiaz <andreas.johansson.dev@gmail.com>
2024-06-19 22:11:40 +0200

    Initial commit. Work in progress.

---
+++ hagadalens-vandrarhem-recreated/README.md
@@ -0,0 +1,6 @@
+## To-do
+
+- Add Cookie consent banner: https://github.com/orestbida/cookieconsent
+- Add language support for static website:
+https://medium.com/@nohanabil/building-a-multilingual-static-website-a-step-by-step-guide-7af238cc8505
+- Create new design using Tailwind CSS: https://www.figma.com/community/file/958383439532195363 and with new colors https://www.realtimecolors.com/?colors=070906-fbfcfa-7EB02D-c2afcc-b58b93&fonts=Archivo-Archivo
---
+++ hagadalens-vandrarhem-recreated/js/site.js
@@ -0,0 +1,27 @@
+﻿$('.setLanguageButton').on('click', function () {
+
+    $this = $(this);
+    var language = $this.data("language");
+
+    $.cookie("Language", language, {path:"/"});
+    document.location.reload();
+    return false;
+});
+
+$('[data-opensitem]').on('click', function () {
+    var $this = $(this);
+    var value = $this.data("opensitem");
+
+    $('[data-opensitem]').removeClass('active');
+    $this.toggleClass('active');
+
+    $('.faqTitle').text($this.text());
+
+    $('[data-item]').hide();
+    $('[data-item=' + value + ']').fadeIn();
+});
+
+$().ready(function () {
+    $('[data-item]').hide();
+    $('[data-opensitem]').first().click();
+});
